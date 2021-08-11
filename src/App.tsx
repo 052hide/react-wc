8bef101d62540a5a1926f6fcf5b2cc810718701a
052hide <hidetoshi1990@gmail.com>
2021-08-11 17:04:06 +0900

    wc読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <wc-button html-type="button" disabled={false} loading={true} theme="primary" size="base" block={true}>
+        Button
+      </wc-button>
     </div>
   );
 }
